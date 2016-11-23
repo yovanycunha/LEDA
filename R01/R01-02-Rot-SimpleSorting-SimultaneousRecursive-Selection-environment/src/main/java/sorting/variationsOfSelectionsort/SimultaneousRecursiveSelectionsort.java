f11730f7bd42d659cd849a55f95884e199c15cdd
yovanycunha <yovany33@gmail.com>
2016-11-23 19:12:13 -0300

    R01 11/23

---
+++ R01/R01-02-Rot-SimpleSorting-SimultaneousRecursive-Selection-environment/src/main/java/sorting/variationsOfSelectionsort/SimultaneousRecursiveSelectionsort.java
@@ -0,0 +1,18 @@
+package sorting.variationsOfSelectionsort;
+
+import sorting.AbstractSorting;
+
+/**
+ * Este algoritmo eh RECURSIVO e aplica o selectionsort na entrada selectionando o 
+ * menor e o maior elemento a cada iteração e colocando eles nas posições corretas. 
+ * Nas proximas iterações o espaço de trabalho do algoritmo deve excluir as posiçoes
+ * dos elementos das iterações anteriores. 
+ */
+public class SimultaneousRecursiveSelectionsort<T extends Comparable<T>> extends
+		AbstractSorting<T> {
+	
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+}
