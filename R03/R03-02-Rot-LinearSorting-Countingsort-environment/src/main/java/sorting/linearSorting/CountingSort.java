961e90a2a0f21ac2987ae99c621f88586be9a3ed
yovanycunha <yovany33@gmail.com>
2016-11-30 20:36:13 -0300

    R03 11/30

--- R03/R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03/R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -12,8 +12,37 @@ public class CountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+
+		//verifica sitacoes extraordinarias
+		if (array.length == 0 || array == null || leftIndex > rightIndex || leftIndex > 0 || rightIndex > array.length) {
+			return;
+		}
+		
+		int maior = encontraMaior(array, leftIndex, rightIndex);
+		int[] contagem = new int[maior + 1];
+		
+		//faz a frequencia dos elementos
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			contagem[array[i]] += 1;
+		}
+		
+		for (int i = 0, j = leftIndex; i < contagem.length; i++) {
+			while (contagem[i] > 0) {
+				array[j] = i;
+				j++;
+				contagem[i]--;
+			}
+		}
+		
 	}
 
+	private int encontraMaior(Integer[] array, int leftIndex, int rightIndex) {
+		int maior = array[leftIndex];
+		for (int i = leftIndex + 1; i <= rightIndex; i++) {
+			if (maior < array[i]) {
+				maior = array[i];
+			}
+		}
+		return maior;
+	}
 }
--- R03/R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03/R03-02-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,8 +12,48 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		//verifica sitacoes extraordinarias
+		if (array.length == 0 || array == null || leftIndex > rightIndex || leftIndex > 0
+				|| rightIndex > array.length) {
+			return;
+		}
+
+		int maior = encontraMaior(array, leftIndex, rightIndex);
+		int menor = encontraMenor(array, leftIndex, rightIndex);
+		int[] contagem = new int[(maior - menor) + 1];
+		
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			contagem[array[i] - menor]++; 
+		}
+		
+		for (int i = 0, j = leftIndex; i < contagem.length; i++) {
+			while (contagem[i] > 0) {
+				array[j] = i + menor;
+				j++;
+				contagem[i]--;
+			}
+		}
+
+	}
+
+	private int encontraMenor(Integer[] array, int leftIndex, int rightIndex) {
+		int menor = array[leftIndex];
+		for (int i = leftIndex + 1; i <= rightIndex; i++) {
+			if (menor > array[i]) {
+				menor = array[i];
+			}
+		}
+		return menor;
+	}
+
+	private int encontraMaior(Integer[] array, int leftIndex, int rightIndex) {
+		int maior = array[leftIndex];
+		for (int i = leftIndex + 1; i <= rightIndex; i++) {
+			if (maior < array[i]) {
+				maior = array[i];
+			}
+		}
+		return maior;
 	}
 
 }
