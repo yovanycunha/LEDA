9907d6215a95d0611c3730b1b6e0d28a477ab355
yovanycunha <yovany33@gmail.com>
2016-12-05 17:31:28 -0300

    R04

--- R04/R04-02-Rot-KLargestOrderStatistics-environment/src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
+++ R04/R04-02-Rot-KLargestOrderStatistics-environment/src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
@@ -1,38 +1,47 @@
 package orderStatistic;
 
+import java.util.Random;
+
+import util.Util;
+
 /**
- * Uma implementacao da interface KLargest que usa estatisticas de ordem para 
+ * Uma implementacao da interface KLargest que usa estatisticas de ordem para
  * retornar um array com os k maiores elementos de um conjunto de dados/array.
  * 
  * A k-esima estatistica de ordem de um conjunto de dados eh o k-esimo menor
- * elemento desse conjunto. Por exemplo, considere o array [4,8,6,9,12,1]. 
- * A 3a estatistica de ordem eh 6, a 6a estatistica de ordem eh 12.
+ * elemento desse conjunto. Por exemplo, considere o array [4,8,6,9,12,1]. A 3a
+ * estatistica de ordem eh 6, a 6a estatistica de ordem eh 12.
  * 
- * Note que, para selecionar os k maiores elementos, pode-se pegar todas as 
- * estatisticas de ordem maiores que k. 
+ * Note que, para selecionar os k maiores elementos, pode-se pegar todas as
+ * estatisticas de ordem maiores que k.
  * 
- * Requisitos do algoritmo:
- * - DEVE ser in-place. Voce pode modificar o array original
- * - Voce DEVE usar alguma ideia de algoritmo de ordenacao visto em sala 
- *   para calcular estatisticas de ordem. 
+ * Requisitos do algoritmo: - DEVE ser in-place. Voce pode modificar o array
+ * original - Voce DEVE usar alguma ideia de algoritmo de ordenacao visto em
+ * sala para calcular estatisticas de ordem.
  * 
  * @author Adalberto
  *
  * @param <T>
  */
-public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLargest<T>{
+public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLargest<T> {
 
 	@Override
 	public T[] getKLargest(T[] array, int k) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
-		//este metodo deve obrigatoriamente usar o orderStatistics abaixo.
+		
+		T estat = orderStatistics(array, k);
+		Integer[] maiores = new Integer[array.length - k];
+		for (int i = k; i < array.length; i++) {
+			maiores[i] = (Integer) array[i];
+		}
+		return (T[]) maiores;
+		//throw new UnsupportedOperationException("Not implemented yet!");
+		// este metodo deve obrigatoriamente usar o orderStatistics abaixo.
 	}
 
 	/**
-	 * Metodo que retorna a k-esima estatistica de ordem de um array, usando
-	 * a ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista 
-	 * a k-esima estatistica de ordem, entao retorna null.
+	 * Metodo que retorna a k-esima estatistica de ordem de um array, usando a
+	 * ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista a
+	 * k-esima estatistica de ordem, entao retorna null.
 	 * 
 	 * Obs: o valor de k deve ser entre 1 e N.
 	 * 
@@ -40,8 +49,35 @@ public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLa
 	 * @param k
 	 * @return
 	 */
-	public T orderStatistics(T[] array, int k){
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");		
+	public T orderStatistics(T[] array, int k) {
+		if (k > array.length || k < 0 || array.length == 0 || array == null) {
+			return null;
+		}
+		
+		quickSort(array, 0, array.length - 1);
+		
+		return array[k - 1];
+
+		// throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	private void quickSort(T[] array, int ini, int fim) {
+		int posicaoPivot = partition(array, ini, fim);
+		quickSort(array, ini, posicaoPivot - 1);
+		quickSort(array, posicaoPivot + 1, fim);
+	}
+
+	private Integer partition(T[] array, Integer ini, Integer fim) {
+		T pivot = array[ini];
+		Integer i = ini;
+		
+		for (int j = i + 1; j <= array.length; j++) {
+			if (array[j].compareTo(pivot) < 0) {
+				i++;
+				Util.swap(array, j, i);
+			}
+		}
+		Util.swap(array, ini, i);
+		return i;
 	}
 }
