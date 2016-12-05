c2ee68a8686fe7f6611cb5f67074d51f469cf954
yovanycunha <yovany33@gmail.com>
2016-12-05 20:07:01 -0300

    R04

--- R04/R04-02-Rot-KLargestOrderStatistics-environment/src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
+++ R04/R04-02-Rot-KLargestOrderStatistics-environment/src/main/java/orderStatistic/KLargestOrderStatisticsImpl.java
@@ -1,7 +1,5 @@
 package orderStatistic;
 
-import java.util.Random;
-
 import util.Util;
 
 /**
@@ -25,59 +23,58 @@ import util.Util;
  */
 public class KLargestOrderStatisticsImpl<T extends Comparable<T>> implements KLargest<T> {
 
-	@Override
-	public T[] getKLargest(T[] array, int k) {
-		
-		T estat = orderStatistics(array, k);
-		Integer[] maiores = new Integer[array.length - k];
-		for (int i = k; i < array.length; i++) {
-			maiores[i] = (Integer) array[i];
-		}
-		return (T[]) maiores;
-		//throw new UnsupportedOperationException("Not implemented yet!");
-		// este metodo deve obrigatoriamente usar o orderStatistics abaixo.
-	}
+   @Override
+   public T[] getKLargest(T[] array, int k) {
 
-	/**
-	 * Metodo que retorna a k-esima estatistica de ordem de um array, usando a
-	 * ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista a
-	 * k-esima estatistica de ordem, entao retorna null.
-	 * 
-	 * Obs: o valor de k deve ser entre 1 e N.
-	 * 
-	 * @param array
-	 * @param k
-	 * @return
-	 */
-	public T orderStatistics(T[] array, int k) {
-		if (k > array.length || k < 0 || array.length == 0 || array == null) {
-			return null;
-		}
-		
-		quickSort(array, 0, array.length - 1);
-		
-		return array[k - 1];
+      T estat = orderStatistics(array, k);
+      Integer[] maiores = new Integer[array.length - k];
+      for (int i = k; i < array.length; i++) {
+         maiores[i] = (Integer) array[i];
+      }
+      return (T[]) maiores;
+      // este metodo deve obrigatoriamente usar o orderStatistics abaixo.
+   }
 
-		// throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   /**
+    * Metodo que retorna a k-esima estatistica de ordem de um array, usando a
+    * ideia de algum algoritmo de ordenacao visto em sala. Caso nao exista a
+    * k-esima estatistica de ordem, entao retorna null.
+    * 
+    * Obs: o valor de k deve ser entre 1 e N.
+    * 
+    * @param array
+    * @param k
+    * @return
+    */
+   public T orderStatistics(T[] array, int k) {
+      if (k > array.length || k < 0 || array.length == 0 || array == null) {
+         return null;
+      }
 
-	private void quickSort(T[] array, int ini, int fim) {
-		int posicaoPivot = partition(array, ini, fim);
-		quickSort(array, ini, posicaoPivot - 1);
-		quickSort(array, posicaoPivot + 1, fim);
-	}
+      quickSort(array, 0, array.length - 1);
 
-	private Integer partition(T[] array, Integer ini, Integer fim) {
-		T pivot = array[ini];
-		Integer i = ini;
-		
-		for (int j = i + 1; j <= array.length; j++) {
-			if (array[j].compareTo(pivot) < 0) {
-				i++;
-				Util.swap(array, j, i);
-			}
-		}
-		Util.swap(array, ini, i);
-		return i;
-	}
+      return array[k - 1];
+   }
+
+   private void quickSort(T[] array, int ini, int fim) {
+      if (array != null && ini < fim) {
+         int posicaoPivot = partition(array, ini, fim);
+         quickSort(array, ini, posicaoPivot - 1);
+         quickSort(array, posicaoPivot + 1, fim);
+      }
+   }
+
+   private Integer partition(T[] array, Integer ini, Integer fim) {
+      T pivot = array[ini];
+      Integer i = ini;
+
+      for (int j = i + 1; j <= array.length; j++) {
+         if (array[j].compareTo(pivot) < 0) {
+            i++;
+            Util.swap(array, j, i);
+         }
+      }
+      Util.swap(array, ini, i);
+      return i;
+   }
 }
