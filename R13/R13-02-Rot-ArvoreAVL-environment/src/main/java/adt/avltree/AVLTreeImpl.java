d9d8234d59629ed112e9bc67e1d63c77f160332d
yovanycunha <yovany33@gmail.com>
2017-02-22 10:31:46 -0300

    lets dance

--- R13/R13-02-Rot-ArvoreAVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
+++ R13/R13-02-Rot-ArvoreAVL-environment/src/main/java/adt/avltree/AVLTreeImpl.java
@@ -1,6 +1,5 @@
 package adt.avltree;
 
-import adt.bst.BST;
 import adt.bst.BSTImpl;
 import adt.bst.BSTNode;
 import adt.bt.Util;
@@ -13,87 +12,86 @@ import adt.bt.Util;
  *
  * @param <T>
  */
-public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements
-		AVLTree<T> {
+public class AVLTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements AVLTree<T> {
 
-	// TODO Do not forget: you must override the methods insert and remove
-	// conveniently.
+   // TODO Do not forget: you must override the methods insert and remove
+   // conveniently.
 
-	// AUXILIARY
-	protected int calculateBalance(BSTNode<T> node) {
-		return node == null || node.isEmpty() ? 
-				0 : height((BSTNode<T>) node.getRight()) - height((BSTNode<T>) node.getLeft());
-	}
+   // AUXILIARY
+   protected int calculateBalance(BSTNode<T> node) {
+      return node == null || node.isEmpty() ? 0 : height((BSTNode<T>) node.getRight())
+            - height((BSTNode<T>) node.getLeft());
+   }
 
-	// AUXILIARY
-	protected void rebalance(BSTNode<T> node) {
-		
-		if (node == null || node.isEmpty()) {
-			return ;
-		}
-		
-		int balance = calculateBalance(node);
-		
-		if (balance < -1) {
-			if (calculateBalance((BSTNode<T>) node.getLeft()) > 0) {
-				this.leftRotation(node);
-			}
-			this.rightRotation(node);			
-		} else {
-			if (calculateBalance((BSTNode<T>) node.getRight()) < 0) {
-				this.rightRotation(node);
-			}
-			this.leftRotation(node);
-		}	
-	}
+   // AUXILIARY
+   protected void rebalance(BSTNode<T> node) {
 
-	protected void rightRotation(BSTNode<T> node) {
+      if (node == null || node.isEmpty()) {
+         return;
+      }
 
-		BSTNode<T> aux = Util.rightRotation(node);
-		
-		if (node.getParent() == null) {
-			root = node;
-		}
-	}
+      int balance = calculateBalance(node);
 
-	protected void leftRotation(BSTNode<T> node) {
+      if (balance < -1) {
+         if (calculateBalance((BSTNode<T>) node.getLeft()) > 0) {
+            this.leftRotation((BSTNode<T>) node.getLeft());
+         }
+         this.rightRotation(node);
+      } else if (balance > 1) {
+         if (calculateBalance((BSTNode<T>) node.getRight()) < 0) {
+            this.rightRotation((BSTNode<T>) node.getRight());
+         }
+         this.leftRotation(node);
+      }
+   }
 
-		BSTNode<T> aux = Util.leftRotation(node);
-		
-		if (node.getParent() == null) {
-			root = node;
-		}
-		
-	}
+   protected void rightRotation(BSTNode<T> node) {
 
-	// AUXILIARY
-	protected void rebalanceUp(BSTNode<T> node) {
+      BSTNode<T> aux = Util.rightRotation(node);
 
-		if (!node.isEmpty()) {
-			rebalance(node);
-			rebalanceUp((BSTNode<T>) node.getParent());
-		}		
-	}
-	
-	@Override
-	public void insert(T element) {
-		insert(element, super.getRoot(), new BSTNode<T>());
-	}
+      if (node.getParent() == null) {
+         root = node;
+      }
+   }
 
-	private void insert(T element, BSTNode<T> node, BSTNode<T> parent) {
-		if (element == null) {
-			return;
-		}
-		
-		if (node.isEmpty()) {
-			node.setData(element);
-			node.setLeft(new BSTNode<T>());
-			node.setRight(new BSTNode<T>());
-			node.setParent(parent);
-		} else if (element.compareTo(node.getData()) < 0) {
-			insert(element, (BSTNode<T>) node.getLeft(), node);
-		} else 
-			insert(element, (BSTNode<T>) node.getRight(), node);
-		rebalance(node);
-	}
+   protected void leftRotation(BSTNode<T> node) {
+
+      BSTNode<T> aux = Util.leftRotation(node);
+
+      if (node.getParent() == null) {
+         root = node;
+      }
+
+   }
+
+   // AUXILIARY
+   protected void rebalanceUp(BSTNode<T> node) {
+
+      if (node != null) {
+         rebalance(node);
+         rebalanceUp((BSTNode<T>) node.getParent());
+      }
+   }
+
+   @Override
+   public void insert(T element) {
+      insert(element, super.getRoot(), new BSTNode<T>());
+   }
+
+   private void insert(T element, BSTNode<T> node, BSTNode<T> parent) {
+      if (element == null) {
+         return;
+      }
+
+      if (node.isEmpty()) {
+         node.setData(element);
+         node.setLeft(new BSTNode<T>());
+         node.setRight(new BSTNode<T>());
+         node.setParent(parent);
+      } else if (element.compareTo(node.getData()) < 0) {
+         insert(element, (BSTNode<T>) node.getLeft(), node);
+      } else
+         insert(element, (BSTNode<T>) node.getRight(), node);
+      rebalance(node);
+   }
 }
